1be592ff29c6f5f4bdbfa196e57fe2c4505cc4e4
Polo <poloaguirre3421@gmail.com>
2022-03-11 19:39:56 -0700

    Inital Commit

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ File: Server.js
 Description: Web API scaffolding for Movie API
  */
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var authController = require('./auth');
-var authJwtController = require('./auth_jwt');
-var jwt = require('jsonwebtoken');
-var cors = require('cors');
-var User = require('./Users');
+var express = require("express");
+var bodyParser = require("body-parser");
+var passport = require("passport");
+var authController = require("./auth");
+var authJwtController = require("./auth_jwt");
+var jwt = require("jsonwebtoken");
+var cors = require("cors");
+var User = require("./Users");
+const Movies = require("./moviesModel");
 
 var app = express();
 app.use(cors());
@@ -23,70 +24,153 @@ app.use(passport.initialize());
 var router = express.Router();
 
 function getJSONObjectForMovieRequirement(req) {
-    var json = {
-        headers: "No headers",
-        key: process.env.UNIQUE_KEY,
-        body: "No body"
-    };
+  var json = {
+    headers: "No headers",
+    key: process.env.UNIQUE_KEY,
+    body: "No body",
+  };
 
-    if (req.body != null) {
-        json.body = req.body;
-    }
+  if (req.body != null) {
+    json.body = req.body;
+  }
 
-    if (req.headers != null) {
-        json.headers = req.headers;
-    }
+  if (req.headers != null) {
+    json.headers = req.headers;
+  }
 
-    return json;
+  return json;
 }
 
-router.post('/signup', function(req, res) {
-    if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please include both username and password to signup.'})
-    } else {
-        var user = new User();
-        user.name = req.body.name;
-        user.username = req.body.username;
-        user.password = req.body.password;
+router.post("/signup", function (req, res) {
+  if (!req.body.username || !req.body.password) {
+    return res.json({
+      success: false,
+      msg: "Please include both username and password to signup.",
+    });
+  } else {
+    var user = new User();
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
 
-        user.save(function(err){
-            if (err) {
-                if (err.code == 11000)
-                    return res.json({ success: false, message: 'A user with that username already exists.'});
-                else
-                    return res.json(err);
-            }
+    user.save(function (err) {
+      if (err) {
+        if (err.code == 11000)
+          return res.json({
+            success: false,
+            message: "A user with that username already exists.",
+          });
+        else return res.json(err);
+      }
 
-            res.json({success: true, msg: 'Successfully created new user.'})
-        });
-    }
+      return res.json({
+        success: true,
+        user,
+        msg: "Successfully created new user.",
+      });
+    });
+  }
 });
 
-router.post('/signin', function (req, res) {
-    var userNew = new User();
-    userNew.username = req.body.username;
-    userNew.password = req.body.password;
+router.post("/signin", function (req, res) {
+  var userNew = new User();
+  userNew.username = req.body.username;
+  userNew.password = req.body.password;
 
-    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
-        if (err) {
-            res.send(err);
+  User.findOne({ username: userNew.username })
+    .select("name username password")
+    .exec(function (err, user) {
+      if (err) {
+        return res.send(err);
+      }
+
+      user.comparePassword(userNew.password, function (isMatch) {
+        if (isMatch) {
+          var userToken = { id: user.id, username: user.username };
+          var token = jwt.sign(userToken, process.env.SECRET_KEY);
+          return res.json({ success: true, user, token: "JWT " + token });
+        } else {
+          return res
+            .status(401)
+            .send({ success: false, msg: "Authentication failed." });
         }
-
-        user.comparePassword(userNew.password, function(isMatch) {
-            if (isMatch) {
-                var userToken = { id: user.id, username: user.username };
-                var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                res.json ({success: true, token: 'JWT ' + token});
-            }
-            else {
-                res.status(401).send({success: false, msg: 'Authentication failed.'});
-            }
-        })
-    })
+      });
+    });
 });
 
-app.use('/', router);
+router.get("/movies", authJwtController.isAuthenticated, async (req, res) => {
+  try {
+    const movies = await Movies.find();
+    return res.status(200).json(movies);
+  } catch (error) {
+    return es.status(500).json(error);
+  }
+});
+
+router.post("/movie", authJwtController.isAuthenticated, (req, res) => {
+  const { title, year, genre, actors } = req.body;
+  if (!actors || actors.length < 3) {
+    return res
+      .status(500)
+      .json({ success: false, msg: "Must have at least 3 actors" });
+  }
+  Movies.create(
+    {
+      title,
+      year,
+      genre,
+      actors,
+    },
+    (error, movie) => {
+      if (error) {
+        return res.status(500).json(error);
+      }
+      return res.status(200).json(movie);
+    }
+  );
+});
+
+router.get(
+  "/movie/:movieID",
+  authJwtController.isAuthenticated,
+  async (req, res) => {
+    try {
+      const movie = await Movies.findById(req.params.movieID);
+      return es.status(200).json(movie);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+);
+router.put("/movie/:movieID", authJwtController.isAuthenticated, (req, res) => {
+  const { title, year, genre, actors } = req.body;
+  if (!actors || actors.length < 3) {
+    return res
+      .status(500)
+      .json({ success: false, msg: "Must have at least 3 actors" });
+  }
+
+  Movies.findByIdAndUpdate(req.params.movieID, {
+    $set: { title: title, year: year, genre: genre, actors: actors },
+  }).exec((error, movie) => {
+    if (error) {
+      return res.status(500).json(error);
+    }
+    return res.status(200).json(movie);
+  });
+});
+router.delete(
+  "/movie:movieID",
+  authJwtController.isAuthenticated,
+  async (req, res) => {
+    try {
+      const movie = await Movies.findByIdAndDelete(req.params.movieID);
+      return res.status(200).json(`${movie.title} has been deleted`);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+);
+app.use("/", router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
-
-
